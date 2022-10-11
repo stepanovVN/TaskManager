99f5d35dcdf9171b880da22485377a28b3739047
stepanovVN <115571666+stepanovVN@users.noreply.github.com>
2022-10-11 16:20:07 +0000

    lesson 2 task

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 #Менеджер задач. Исходный код на Python
-HELP="""Менеджер задач от Степанова В.Н. Используйте комманды:
+HELP = """Менеджер задач от Степанова В.Н. Используйте комманды:
 add - добавить задачу
 show - показать список активных задач
 help - вывод этой информации
 """
-tasks=[] #список задач
+tasks = []  #списки задач
+today=[]
+tomorrow=[]
+other=[]
 while True:
-  command=input("Введите команду: ")
-  if command=="add":
-    tasks.append(input("Введите задачу: "))
+  command = input("Введите команду: ")
+  if command == "add":
+    task=input("    Введите задачу: ")
+    date=input("    Дата (сегодня, завтра, иное ?: ")
+    if date=="сегодня":
+      today.append(task)
+    elif date=="завтра":
+      tomorrow.append(task)
+    else:
+      other.append(task)
     print("    Задача добавлена")
-  elif command=="show":
-    print(tasks)
-  elif command=="help":
+  elif command == "show":
+    print("Сегодня: \n",today)
+    print("Завтра: \n",tomorrow)
+    print("Иные задачи: \n",other)
+  elif command == "help":
     print(HELP)
-  elif command=="exit": 
+  elif command == "exit":
     print("До свидания !")
     break
+  else:
+    print("    Неизвестная команда")
