e5b1811b62c0bc4709d516f8e694755791c6e31d
stepanovVN <115571666+stepanovVN@users.noreply.github.com>
2022-10-11 15:51:11 +0000

    first release

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-tasks=[]
-while true:
+#Менеджер задач. Исходный код на Python
+HELP="""Менеджер задач от Степанова В.Н. Используйте комманды:
+add - добавить задачу
+show - показать список активных задач
+help - вывод этой информации
+"""
+tasks=[] #список задач
+while True:
+  command=input("Введите команду: ")
+  if command=="add":
+    tasks.append(input("Введите задачу: "))
+    print("    Задача добавлена")
+  elif command=="show":
+    print(tasks)
+  elif command=="help":
+    print(HELP)
+  elif command=="exit": 
+    print("До свидания !")
+    break
